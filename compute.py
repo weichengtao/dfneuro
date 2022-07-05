bbdf3a7317d93964f34a917dce17ece3c02f2a3e
Weicheng Tao <warr.tao@gmail.com>
2022-07-06 00:25:39 +0800

    Generate shuffled bursts

--- compute.py
+++ compute.py
@@ -202,8 +202,31 @@ def combine_burst(burst_list: list[list[tuple[int, int]]], epoch_samples: int, w
         res = burst(sig, wmin, 0.5)[0]
     return res
 
-def state_duration(burst: list[tuple[int, int]]) -> int:
-    return np.sum([b[1] - b[0] + 1 for b in burst])
+def state_duration(bursts: list[tuple[int, int]]) -> int:
+    return np.sum([b[1] - b[0] + 1 for b in bursts])
+
+def shuffle_burst(bursts: list[tuple[int, int]], epoch_samples: int, rng: int | np.random.RandomState | None = None) -> list[tuple[int, int]]:
+    bursts = bursts[:]
+    n_burst = len(bursts)
+    burst_duration = state_duration(bursts)
+    off_burst_duration = epoch_samples - burst_duration
+    total = off_burst_duration + 1
+    # choose n_burst samples from total
+    if not isinstance(rng, np.random.RandomState):
+        rng = np.random.RandomState(rng)
+    slots = np.sort(rng.choice(total, n_burst, replace=False))
+    # shuffle the order of bursts
+    rng.shuffle(bursts)
+    # insert burst before those samples
+    sig = np.zeros(epoch_samples)
+    offset = 0
+    for i, b in enumerate(bursts):
+        s = slots[i]
+        b_duration = b[1] - b[0] + 1
+        start, end = offset + s, offset + s + b_duration
+        sig[start:end] = 1
+        offset = offset + b_duration
+    return burst(sig, 0, 0.5)[0]
 
 def active_silent(Sxx: np.ndarray, bands: list[tuple[int | float, int | float]], active_sd: int | float, silent_sd: int | float, 
     i_trial: int, offset_samples: int = 0, return_duration: bool = False) -> tuple[list[tuple[int, int]], list[tuple[int, int]]] | tuple[int, int]:
